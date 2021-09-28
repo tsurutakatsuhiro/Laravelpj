937123ce2e32d24ef3bbda70880ba9e4a1321c67
tsurutakatsuhiro <79049222+tsurutakatsuhiro@users.noreply.github.com>
2021-09-28 20:46:18 +0900

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>COACHTECH</title>
+</head>
+
+<body>
+  <form>
+          <h1>テスト</h1>
+          <div>
+            <input type="text" placeholder="氏名"/>
+          </div>
+          <div>
+            <<input type="text" placeholder="メールアドレス"/>
+          </div>
+
+      </form>
+</body>
+
+</html>
